a790baae58b1163db84ba4f1a8e48a0a5c1ea01e
j-zelek <jerome@zelek.biz>
2024-02-09 18:49:30 +0100

    add token fetcher

---
+++ MusicTrackerService/Authentication/AuthenticationHttpClientExtensions.cs
@@ -0,0 +1,26 @@
+﻿using MusicTrackerService.Options;
+
+namespace MusicTrackerService.Authentication
+{
+    public static class AuthenticationHttpClientExtensions
+    {
+        private static void AddTokenFetcherToClient(IHttpClientBuilder httpClientBuilder)
+        {
+
+            httpClientBuilder.AddHttpMessageHandler<TokenFetcherHttpClientHandler>();
+        }
+
+        private static void AddTokenAuthentication(this IServiceCollection services, TokenFetcherOptions options, IHttpClientBuilder httpClientBuilder)
+        {
+            services.Configure<TokenFetcherOptions>(o =>
+            {
+                o.ClientId = options.ClientId;
+                o.ClientSecret = options.ClientSecret;  
+                o.TokenEndpoint = options.TokenEndpoint;
+            });
+            services.AddTransient<TokenFetcherHttpClientHandler>();
+            services.AddHttpClient<TokenFetcher>();
+            AddTokenFetcherToClient(httpClientBuilder);
+        }
+    }
+}
---
+++ MusicTrackerService/Authentication/TokenFetcherHttpClientHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Headers;
+
+namespace MusicTrackerService.Authentication
+{
+    internal class TokenFetcherHttpClientHandler : DelegatingHandler
+    {
+        private readonly TokenFetcher _tokenFetcher;
+
+        public TokenFetcherHttpClientHandler(TokenFetcher tokenFetcher)
+        {
+            _tokenFetcher = tokenFetcher;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            await AddTokenCredentialAuthorizationHeader(request).ConfigureAwait(false);
+            return await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+        }
+
+        private async Task AddTokenCredentialAuthorizationHeader(HttpRequestMessage request)
+        {
+            var token = await _tokenFetcher.GetAccessTokenAsync().ConfigureAwait(false);
+            if (token != null) request.Headers.Authorization = new AuthenticationHeaderValue($"{token.TokenType} {token.AccessToken}");
+        }
+    }
+}
